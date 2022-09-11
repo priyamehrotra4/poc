3b44abb4f5aca0f953fcb41419fd4683b9767920
Priya Mehrotra <81843572+priyamehrotra4@users.noreply.github.com>
2022-09-12 01:52:24 +0530

    added graphql POC

---
+++ spring-boot-graphql-query-example/src/main/java/com/graphql/demo/service/datafetcher/BookDataFetcher.java
@@ -0,0 +1,23 @@
+package com.graphql.demo.service.datafetcher;
+
+import com.graphql.demo.model.Book;
+import com.graphql.demo.repository.BookRepository;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookDataFetcher implements DataFetcher<Book>{
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Override
+    public Book get(DataFetchingEnvironment dataFetchingEnvironment) {
+
+        String isn = dataFetchingEnvironment.getArgument("id");
+
+        return bookRepository.findById(isn).get();
+    }
+}
